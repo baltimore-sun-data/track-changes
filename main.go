c94f1db98840fc8a9b177a6b829a946e2ba5972d
Carl Johnson <me@carlmjohnson.net>
2017-11-13 16:18:19 -0500

    Stub out server

--- main.go
+++ main.go
@@ -1,29 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"strings"
+	"sync"
 
 	"golang.org/x/net/html"
 
 	"github.com/andybalholm/cascadia"
+	"github.com/carlmjohnson/gracefulserver"
 	"github.com/pkg/errors"
 )
 
 func main() {
 	flag.Parse()
-	url := flag.Arg(0)
-	selector := flag.Arg(1)
+	id := flag.Arg(0)
+	url := flag.Arg(1)
+	selector := flag.Arg(2)
 
-	if txt, err := get(url, selector); err != nil {
+	txt, err := get(url, selector)
+	if err != nil {
 		fmt.Fprintf(os.Stderr, "Fatal error: %v\n", err)
 		os.Exit(1)
-	} else {
-		fmt.Println(txt)
 	}
+
+	data.Lock()
+	data.m[id] = txt
+	data.Unlock()
+
+	http.HandleFunc("/", handler)
+	gracefulserver.Serve(http.DefaultServeMux)
 }
 
 func get(url, selector string) (string, error) {
@@ -62,5 +73,26 @@ func deferClose(err *error, f func() error) {
 	newErr := f()
 	if *err == nil {
 		*err = newErr
+	}
+}
+
+var data = struct {
+	m map[string]string
+	sync.RWMutex
+}{
+	m: make(map[string]string),
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	e := json.NewEncoder(w)
+	data.RLock()
+	defer data.RUnlock()
+	if err := e.Encode(&data.m); err != nil {
+		log.Printf("Unexpected error: %v", err)
 	}
 }
