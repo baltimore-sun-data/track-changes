4ba65ac4c15a34f64b23b57b7fcb7bb0ebfd064b
Carl Johnson <me@carlmjohnson.net>
2017-12-01 15:51:20 -0500

    Webpack: Don't hash filenames in watch mode

--- webpack.config.js
+++ webpack.config.js
@@ -5,65 +5,69 @@ const MinifyPlugin = require("babel-minify-webpack-plugin");
 const path = require("path");
 const webpack = require("webpack");
 
-const extractSass = new ExtractTextPlugin({
-  filename: "css/[name].[hash].css"
-});
+module.exports = (env = {}) => {
+  let namePattern = env.production ? "[name].[hash]" : "[name]";
 
-module.exports = {
-  context: path.resolve(__dirname, "src"),
-  entry: "./js/app.js",
-  output: {
-    filename: "js/[name].[hash].js",
-    path: path.resolve(__dirname, "assets/static")
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["es2016"]
-          }
-        }
-      },
-      {
-        test: /\.scss$/,
-        use: extractSass.extract({
-          use: [
-            {
-              loader: "css-loader"
-            },
-            {
-              loader: "postcss-loader",
-              options: {
-                ident: "postcss",
-                plugins: loader => [
-                  require("postcss-import")({ root: loader.resourcePath }),
-                  require("autoprefixer")(),
-                  require("cssnano")()
-                ]
-              }
-            },
-            {
-              loader: "sass-loader"
+  const extractSass = new ExtractTextPlugin({
+    filename: `css/${namePattern}.css`
+  });
+
+  return {
+    context: path.resolve(__dirname, "src"),
+    entry: "./js/app.js",
+    output: {
+      filename: `js/${namePattern}.js`,
+      path: path.resolve(__dirname, "assets/static")
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["es2016"]
             }
-          ],
-          // use style-loader in development
-          fallback: "style-loader"
-        })
-      }
+          }
+        },
+        {
+          test: /\.scss$/,
+          use: extractSass.extract({
+            use: [
+              {
+                loader: "css-loader"
+              },
+              {
+                loader: "postcss-loader",
+                options: {
+                  ident: "postcss",
+                  plugins: loader => [
+                    require("postcss-import")({ root: loader.resourcePath }),
+                    require("autoprefixer")(),
+                    require("cssnano")()
+                  ]
+                }
+              },
+              {
+                loader: "sass-loader"
+              }
+            ],
+            // use style-loader in development
+            fallback: "style-loader"
+          })
+        }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(["assets"]),
+      new ManifestPlugin({
+        fileName: "../manifest.json",
+        publicPath: "/static/"
+      }),
+      new MinifyPlugin(),
+      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+      extractSass
     ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(["assets"]),
-    new ManifestPlugin({
-      fileName: "../manifest.json",
-      publicPath: "/static/"
-    }),
-    new MinifyPlugin(),
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    extractSass
-  ]
+  };
 };
